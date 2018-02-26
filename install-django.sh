b193ed6b34f17b55ec95e1cebddefd08ce4b1a03
Gustavo Martínez <tavo962@gmail.com>
2018-02-25 22:30:37 -0600

    Progress

---
+++ install-django.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+# ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+# ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+# ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+# ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+#  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+
+clear
+echo -e "\n ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝"
+
+echo -e "\nHostname:"
+read host
+
+function base() {
+	echo -e "\n\nStart\nWeb Server:\t\t\t\t\t$daemon"
+	echo -e "Base configuration\t\t\t\tOK"
+
+	apt install language-pack-UTF-8
+	locale-gen en_US.UTF-8
+	touch /var/lib/cloud/instance/locale-check.skip
+	
+	apt update -y
+	apt upgrade -y
+	# apt dist-upgrade -y
+	dpkg-reconfigure tzdata
+	apt install curl -y
+	apt install zip -y
+
+	echo -e "$host\n" > /etc/hostname
+}	# end function
+
+# base
+# apt install nginx -y
+# service nginx start
